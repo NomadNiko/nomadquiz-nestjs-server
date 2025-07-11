57a52f7e5aeebb98dcf97068da944c6a4723777a
NomadNiko <nomad.niko.inc@gmail.com>
2025-07-11 00:46:36 -1000

    fix: removing validation on usernames to allow email as username

--- src/friends/dto/send-friend-request.dto.ts
+++ src/friends/dto/send-friend-request.dto.ts
@@ -5,11 +5,11 @@ export class SendFriendRequestDto {
   @ApiProperty({
     type: String,
     example: 'john_doe',
-    description: 'Username of the user to send friend request to',
+    description: 'Username (or email for users who haven\'t changed their username) of the user to send friend request to',
   })
   @IsNotEmpty()
   @IsString()
   @MinLength(1)
-  @MaxLength(50)
+  @MaxLength(255) // Increased to support email addresses as usernames
   recipientUsername: string;
 }
